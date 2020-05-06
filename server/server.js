39adffbf31375a7b246d813716660dacf8c50bb5
Juan Pablo Tyrlik <juan.pablo.tyrlik@gmail.com>
2020-05-05 23:03:27 -0300

    SocketIO

--- server/server.js
+++ server/server.js
@@ -1,17 +1,26 @@
 const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
+
 
 const path = require('path');
 
 const app = express();
+const server = http.createServer(app);
 
 const publicPath = path.resolve(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
+// Comunicacion con backend
+const io = socketIO(server);
 
+io.on('connection', (client) => {
+    console.log('Usuario conectado');
+});
 
-app.listen(port, (err) => {
+server.listen(port, (err) => {
 
     if (err) throw new Error(err);
 

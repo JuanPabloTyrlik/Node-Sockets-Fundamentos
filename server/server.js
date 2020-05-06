70d9833b07d376dc8f6da38be37e2de5b8ab73de
Juan Pablo Tyrlik <juan.pablo.tyrlik@gmail.com>
2020-05-05 23:44:14 -0300

    Ejemplo terminado

--- server/server.js
+++ server/server.js
@@ -18,6 +18,29 @@ const io = socketIO(server);
 
 io.on('connection', (client) => {
     console.log('Usuario conectado');
+    // Escuchar eventos del cliente
+    client.on('disconnect', () => {
+        console.log('Usuario desconectado');
+    });
+    client.on('enviarMensaje', (mensaje, callback) => {
+        console.log('Cliente:', mensaje);
+        if (mensaje.usuario) {
+            callback({
+                ok: true,
+                mensaje: 'Salió todo bien'
+            });
+        } else {
+            callback({
+                ok: false,
+                mensaje: 'Algo salió mal'
+            });
+        }
+    });
+    // Enviar evento
+    client.emit('enviarMensaje', {
+        usuario: 'Administrador',
+        mensaje: 'Bienvenido a la aplicación'
+    });
 });
 
 server.listen(port, (err) => {
